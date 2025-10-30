c50616938e14909a9e11939bf0d200e2ad26bc1b
Fran Núñez <33930681+trexment@users.noreply.github.com>
2025-10-30 13:30:42 +0100

    Clear SMTP configuration in config.php
    Updated email configuration with empty values.

--- app/config/config.php
+++ app/config/config.php
@@ -7,19 +7,11 @@ define('DB_PASS', 'Sign@turit26');
 define('BASE_URL', 'https://recibos.frannunez.es/');
 define('REGISTRATION_TOKEN', 'MI_TOKEN_SECRETO_2025');
 
-// SMTP para enviar invitaciones
-// define('MAIL_HOST', 'frannunez.es');
-// define('MAIL_USER', 'info@frannunez.es');
-// define('MAIL_PASS', 'Sign@turit26');
-// define('MAIL_PORT', 465);
-// define('MAIL_FROM', 'info@frannunez.es');
-// define('MAIL_FROM_NAME', 'Recibos Arbitrales');
-
-define('MAIL_HOST', 'smtp.gmail.com');
-define('MAIL_USER', 'frannunez.dev@gmail.com');
-define('MAIL_PASS', 'advl tcpy qmgx dins');
-define('MAIL_PORT', 587);
-define('MAIL_FROM', 'frannunez.dev@gmail.com');
-define('MAIL_FROM_NAME', 'Recibos Arbitrales');
+define('MAIL_HOST', '');
+define('MAIL_USER', '');
+define('MAIL_PASS', '');
+define('MAIL_PORT', );
+define('MAIL_FROM', '');
+define('MAIL_FROM_NAME', '');
 
 
